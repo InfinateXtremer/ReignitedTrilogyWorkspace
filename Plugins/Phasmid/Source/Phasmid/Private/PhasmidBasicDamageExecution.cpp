edfd56137dd948fdbac5c18963a9e1dc6383c3af
ARMAN-LAPTOP\arman <armanhacikoglu@gmail.com>
2022-03-21 20:21:40 +0300

    no message

---
+++ Plugins/Phasmid/Source/Phasmid/Private/PhasmidBasicDamageExecution.cpp
@@ -0,0 +1,7 @@
+
+
+#include "PhasmidBasicDamageExecution.h"
+
+
+
+
---
+++ Plugins/Phasmid/Source/Phasmid/Public/PhasmidBasicDamageExecution.h
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "PhasmidBasicDamageExecution.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHASMID_API UPhasmidBasicDamageExecution : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
