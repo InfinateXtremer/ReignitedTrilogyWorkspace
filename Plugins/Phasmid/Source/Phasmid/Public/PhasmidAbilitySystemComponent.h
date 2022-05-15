aa20c016c66847b205a9a05a68a39d84beb85251
DESKTOP-UR0HPLL\Arman <armanhacikoglu@gmail.com>
2022-05-16 01:13:27 +0300

    Added sparx health detection utility, renamed flamecolor data table to utilities to include sparx health.

---
+++ Plugins/Phasmid/Source/Phasmid/Public/PhasmidAbilitySystemComponent.h
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "PhasmidAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHASMID_API UPhasmidAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
